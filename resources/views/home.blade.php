b15e996607b44ff734ceb082cd4b9425bc0b78ea
carlosvillar76 <cvillarce15dw@ikzubirimanteo.com>
2016-12-20 11:45:48 +0000

    landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,106 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+      <!--Import materialize.css-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+      <link rel="stylesheet" href="/css/style.css" type="text/css" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="bower_components/mdi/css/materialdesignicons.min.css" media="all" rel="stylesheet" type="text/css" />
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+      <script type="text/javascript" src="/js/home.js"></script>
+      
+      <div id="index-banner" class="parallax-container">
+        <div id="botonLogin">
+          <a class="waves-effect waves-teal btn-flat right">Login</a>
+        </div>
+        
+         <div class="parallax">
+            <img class="valign-wrapper img-responsive" src="/img/Fondo.jpg"></img>
+         </div>
+         <img class="displayed img-responsive" src="/img/Logo_Oficial.png"></img>
+         <p>La vida en el hogar, mas sencilla</p>
+      </div>
+      
+      <div class="red lighten-3">
+        <div class="row">
+          <img class="imagesDis responive-img circle col s4" src="/img/Ejemplo_Disp.jpg"></img>
+          <p class="left col s8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, mollitia, voluptates, eius excepturi perferendis facere corporis sint repellendus optio ipsa delectus saepe beatae labore quae perspiciatis suscipit inventore cupiditate harum.</p>
+        </div>
+      
+        <div class="row">
+          <p class="col s8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, mollitia, voluptates, eius excepturi perferendis facere corporis sint repellendus optio ipsa delectus saepe beatae labore quae perspiciatis suscipit inventore cupiditate harum.</p>
+          <img class="imagesDis responive-img circle col s4" src="/img/Ejemplo_Disp.jpg"></img>
+        </div>
+      
+        <div class="row">
+          <img class="imagesDis responive-img circle col s4" src="/img/Ejemplo_Disp.jpg"></img>
+          <p class="left col s8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, mollitia, voluptates, eius excepturi perferendis facere corporis sint repellendus optio ipsa delectus saepe beatae labore quae perspiciatis suscipit inventore cupiditate harum.</p>
+        </div>
+      </div>
+      
+      
+      <div class="carousel carousel-slider center" data-indicators="true">
+        <div class="carousel-fixed-item center">
+          <a class="btn waves-effect white grey-text darken-text-2">button</a>
+        </div>
+        
+        <div class="carousel-item red white-text" href="#one!">
+          <h2>¿Que ofrecemos?</h2>
+          <p class="white-text"></p>
+          <p>Hola</p>
+        </div>
+        
+        <div class="carousel-item amber white-text" href="#two!">
+          <h2>Second Panel</h2>
+          <p class="white-text">This is your second panel</p>
+        </div>
+        
+        <div class="carousel-item green white-text" href="#three!">
+          <h2>Third Panel</h2>
+          <p class="white-text">This is your third panel</p>
+        </div>
+        
+        <div class="carousel-item blue white-text" href="#four!">
+          <h2>Fourth Panel</h2>
+          <p class="white-text">This is your fourth panel</p>
+        </div>
+      </div>
+      
+      <footer class="page-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Contacta con nosotros:</h5>
+              <p class="grey-text text-lighten-4">Si tiene alguna duda o sugerencia, no dude en decírnoslo.</p>
+              <p class="grey-text text-lighten-4">Telefono: 666 777 888</p>
+              <p class="grey-text text-lighten-4">E-mail: domohelpproject@gmail.com</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Encuentranos en:</h5>
+              <ul>
+                <li><a target="_blank" href="https://www.facebook.com/DomoHelp-1406899652667203/"><i class="mdi mdi-facebook-box mdi-36px"></i></a></li>
+                <li><a target="_blank" href="https://twitter.com/domo_help"><i class="mdi mdi-twitter-circle mdi-36px"></i></a><a>@domo_help</a></li>
+                <li><a target="_blank"href="https://plus.google.com/u/0/118116514461418291407"><i class="mdi mdi-google-plus-box mdi-36px"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright center">
+          <div class="container">
+          © 2016 Copyright DomoHelp
+          </div>
+        </div>
+      </footer>
+    
+    </body>
+  </html>
