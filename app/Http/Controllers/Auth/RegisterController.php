94e0e4fffb0b8953f3eec9905400fcd9b3d5205e
carlosvillar76 <cvillarce15dw@ikzubirimanteo.com>
2017-01-27 12:27:32 +0000

    27/01/2017 Confirmation email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -77,7 +77,8 @@ class RegisterController extends Controller
         $name = $data['name'];
         Mail::send('03_mails.registro', $user, function($message)
     {
-        $message->from('domohelpproject@gmail.com', 'Admin')->subject('Registro DomoHelp')->to($user, $name);
+        $message->from('domohelpproject@gmail.com', 'Admin');
+        $message->to($user, $name)->subject('Registro DomoHelp');
     
     });
         
